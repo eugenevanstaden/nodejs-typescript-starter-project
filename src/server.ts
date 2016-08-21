724af2c7931487a0f4ebb8df232b3086b20e9bf5
Thomas Vercoutre <thomas.vercoutre@live.fr>
2016-08-22 09:37:49 +1200

    Add debugger

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@
 import express = require("express");
 import http = require("http");
 
-let app = express();
+const debug = require("debug")("app-start");
+const app = express();
+
+
 app.set("port", process.env.PORT || 5000);
 
 http.createServer(app).listen(this.app.get("port"), () => {
-    console.log("Server listening on port 5000");
+    debug("Server listening on port 5000");
 });
 
